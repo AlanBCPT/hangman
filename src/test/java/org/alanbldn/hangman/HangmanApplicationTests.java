d1306f9670e94027d07803d233eb0129efe0da1a
AlanBLDN <noreply@example.com>
2026-07-25 16:27:08 +0100

    Initial commit: Spring Boot scaffold before Hangman implementation

---
+++ src/test/java/org/alanbldn/hangman/HangmanApplicationTests.java
@@ -0,0 +1,11 @@
+package org.alanbldn.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
